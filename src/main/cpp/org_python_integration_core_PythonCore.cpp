5eb8b83c3bd598db4450ab0a1aa6060dd177450e
kate <ikaterina0909@gmail.com>
2025-05-16 13:17:56 +0300

    implement modules import support in python interpreter

--- src/main/cpp/org_python_integration_core_PythonCore.cpp
+++ src/main/cpp/org_python_integration_core_PythonCore.cpp
@@ -2,6 +2,7 @@
 #include "headers/globals.h"
 #include "headers/python_object_factory.h"
 #include <Python.h>
+#include <map>
 
 JNIEXPORT jobject JNICALL Java_org_python_integration_core_PythonCore_evaluate(JNIEnv *env, jclass, jstring java_repr) {
     PyObject *main_module = PyImport_AddModule("__main__");
@@ -40,4 +41,84 @@ JNIEXPORT jobject JNICALL Java_org_python_integration_core_PythonCore_evaluate(J
 
 JNIEXPORT void JNICALL Java_org_python_integration_core_PythonCore_free(JNIEnv *env, jclass, jobject java_object) {
     object_manager->free_object(env, java_object);
+}
+
+jobject import_module(JNIEnv *env, jstring java_module) {
+    if (!java_module) {
+        jthrowable java_exception = create_native_operation_exception(env, "Module name cannot be null");
+        env->Throw(java_exception);
+        return nullptr;
+    }
+
+    const char *module_name = env->GetStringUTFChars(java_module, nullptr);
+
+    PyObject *py_module = PyImport_ImportModule(module_name);
+    if (!py_module) {
+        jthrowable java_exception = create_python_exception(env);
+        env->Throw(java_exception);
+        env->ReleaseStringUTFChars(java_module, module_name);
+        return nullptr;
+    }
+    env->ReleaseStringUTFChars(java_module, module_name);
+    return convert_to_python_object(env, py_module);
+}
+
+JNIEXPORT jobject JNICALL
+Java_org_python_integration_core_PythonCore_importModule(JNIEnv *env, jclass, jstring java_module) {
+    return import_module(env, java_module);
+}
+
+JNIEXPORT jobject JNICALL Java_org_python_integration_core_PythonCore_fromImport(
+    JNIEnv *env,
+    jclass,
+    jstring java_from,
+    jobjectArray java_names
+) {
+    if (!java_names) {
+        jthrowable java_exception = create_native_operation_exception(env, "names cannot be null");
+        env->Throw(java_exception);
+        return nullptr;
+    }
+
+    const std::size_t names_size = (std::size_t)env->GetArrayLength(java_names);
+    if (names_size < 1) {
+        jthrowable java_exception = create_native_operation_exception(env, "Must be at least one name");
+        env->Throw(java_exception);
+        return nullptr;
+    }
+
+    jobject java_module = import_module(env, java_from);
+    if (!java_module) {
+        return nullptr;
+    }
+    PyObject *py_module = object_manager->get_object(env, java_module);
+
+    jclass map_class = env->FindClass("java/util/HashMap");
+    jmethodID map_constructor = env->GetMethodID(map_class, "<init>", "(I)V");
+    jobject map = env->NewObject(map_class, map_constructor, (jsize)names_size);
+    jmethodID map_put = env->GetMethodID(map_class, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+
+    std::map<jstring, PyObject *> names;
+    for (std::size_t i = 0; i < names_size; ++i) {
+        jstring java_name = (jstring)env->GetObjectArrayElement(java_names, (jsize)i);
+        if (!java_name) {
+            jthrowable java_exception = create_native_operation_exception(env, "Name cannot be null");
+            env->Throw(java_exception);
+            return nullptr;
+        }
+        const char *name = env->GetStringUTFChars(java_name, nullptr);
+        PyObject *py_attr = PyObject_GetAttrString(py_module, name);
+        env->ReleaseStringUTFChars(java_name, name);
+        if (!py_attr) {
+            jthrowable java_exception = create_python_exception(env);
+            env->Throw(java_exception);
+            return nullptr;
+        }
+        names.insert({java_name, py_attr});
+    }
+
+    for (auto name_and_attr : names) {
+        env->CallObjectMethod(map, map_put, name_and_attr.first, convert_to_python_object(env, name_and_attr.second));
+    }
+    return map;
 }
--- src/test/java/org/python/integration/PythonCoreTest.java
+++ src/test/java/org/python/integration/PythonCoreTest.java
@@ -9,8 +9,12 @@ import org.python.integration.core.PythonSession;
 import org.python.integration.exception.PythonException;
 import org.python.integration.object.IPythonObject;
 
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PythonCoreTest {
 
@@ -37,10 +41,33 @@ public class PythonCoreTest {
         assertThrows(PythonException.class, () -> PythonCore.evaluate("[1, 2, 3"));
     }
 
+
     @Test
     @DisplayName("Should free PythonObject")
     @Disabled("TODO: implement test for free()")
     void shouldCleanUpResourcesWhenFreed() {
         // TODO: implement test for free()
+    }
+
+
+    @Test
+    @DisplayName("Should import module by name")
+    void shouldSuccessfullyImportModule() {
+        IPythonObject module = PythonCore.importModule("math");
+        assertNotNull(module);
+        assertTrue(module.toString().contains("math"));
+    }
+
+    @Test
+    @DisplayName("Should import submodules by names")
+    void shouldSuccessfullyImportSubmodulesFromModule() {
+        Map<String, IPythonObject> submodules = PythonCore.fromImport("math", "tan", "sin", "cos");
+        assertNotNull(submodules);
+        for (var entry : submodules.entrySet()) {
+            assertNotNull(entry.getKey());
+            assertTrue(Set.of("tan", "sin", "cos").contains(entry.getKey()));
+            assertNotNull(entry.getValue());
+            assertTrue(Set.of("tan", "sin", "cos").contains(entry.getValue().toString().substring(19, 22)));
+        }
     }
 }
