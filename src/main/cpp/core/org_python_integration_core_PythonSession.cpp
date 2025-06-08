b1a1de165cc3be18683cfd65483c54b8885366d4
kate <ikaterina0909@gmail.com>
2025-06-09 00:08:52 +0300

    disable concurrent sessions

--- src/main/cpp/core/org_python_integration_core_PythonSession.cpp
+++ src/main/cpp/core/org_python_integration_core_PythonSession.cpp
@@ -1,9 +1,14 @@
 #include "org_python_integration_core_PythonSession.h"
 #include "globals.h"
 #include "python_object_manager.h"
+#include "traits.h"
 #include <Python.h>
 
-JNIEXPORT void JNICALL Java_org_python_integration_core_PythonSession_initializePy(JNIEnv *, jobject) {
+JNIEXPORT void JNICALL Java_org_python_integration_core_PythonSession_initializePy(JNIEnv *env, jobject) {
+    if (Py_IsInitialized()) {
+        env->Throw(java_traits<native_operation_exception>::create(env, "There is already an open session"));
+        return;
+    }
     Py_Initialize();
     Py_UNBLOCK_THREADS object_manager = new PythonObjectManager();
 }
